b261580e9f45e7f15c17a9f74b2dbd98887aa47a
Rahul Pradhan <Rahul Pradhan@RAHUL-LAPTOP>
2021-05-12 20:30:15 +0530

    TestClass06.java commit

--- src/CoreJavaPkg/TestClass06.java
+++ src/CoreJavaPkg/TestClass06.java
@@ -1,7 +1,13 @@
-
+//Core Java program to calculate the frequency of characters in a string
+//Ex. Input String = "Selenium"
+//Ex. Output = "S=1,e=2,l=1,n=1,i=1,u=1,m=1"
 
 package CoreJavaPkg;
 
 public class TestClass06 {
+	
+	public static void main(String args[]) {
+		
+	}
 
 }
