50026bcc3da435f397d9bd32af7f0b4232fc9acd
Rahul Pradhan <rahul_pradhan@outlook.com>
2021-06-03 20:31:32 +0530

    Program to display the pattern like right angle triangle for a character

---
+++ src/CoreJavaPkg/TestClass13.java
@@ -0,0 +1,30 @@
+//Core Java program to display the pattern like right angle triangle for a character
+//Ex. Input char = "*", number of rows = 3
+//Ex. Output line 1 = "*", line 2 = "**", line 3 = "***"
+
+package CoreJavaPkg;
+
+public class TestClass13 {
+
+	public static void main(String[] args) {
+		//The character to print
+		String chToPrint = "A.";
+		
+		//Number of rows to print
+		int numOfRows = 6;
+		
+		//Outer loop from 1 to numOfRows - controls the rows
+		for(int i=1;i<=numOfRows;i++) {
+			
+			//Inner loop to print the chToPrint in the ith row
+			for(int j=1;j<=i;j++) {
+				
+				//Print chToPrint (without new line)
+				System.out.print(chToPrint);
+			}
+			
+			//Print a new line for the next i
+			System.out.println();
+		}
+	}
+}
