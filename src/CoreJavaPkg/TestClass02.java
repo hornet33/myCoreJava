2672d44c8a56f2a522e796d1032144d7a15fd26a
Rahul Pradhan <rahul_pradhan@outlook.com>
2021-06-03 19:55:21 +0530

    Program to reverse the characters of a string

--- src/CoreJavaPkg/TestClass02.java
+++ src/CoreJavaPkg/TestClass02.java
@@ -7,45 +7,45 @@ package CoreJavaPkg;
 public class TestClass02 {
 
 	public static void main(String[] args) {
-		String t = "Sandeep Ashok Burte";
-		String[] o = null;	
+		String input = "Sandeep Ashok Burte"; 
+		String[] output = null;	
 		String p;
 		
-		System.out.println("Input: " + t);
+		System.out.println("Input: " + input);
 		
 		//Check if input string contains any space
-		if(t.contains(" ")) {
+		if(input.contains(" ")) {
 			//If yes, split the input string on the " " (space) delimiter which will return an array of strings stored in o[]
-			o = t.split(" "); 
+			output = input.split("\s"); 
 			
 			//Reset t 
-			t = "";
+			input = "";
 			
 			 //Loop through the length of string array o[] 
-			 for (int i =0; i<o.length;i++) { 
+			 for (int i =0; i<output.length;i++) { 
 				 //For each word in string array o[], navigate the characters of the string in reverse
 				 //That is, from the last to the first character, and keep adding it to var 't'
-				 for (int j = o[i].length()-1;j>=0;j--) {
-					 t = t + o[i].charAt(j); 
+				 for (int j = output[i].length()-1;j>=0;j--) {
+					 input = input + output[i].charAt(j); 
 					 
 					 //If index j = 0 then it is at the end of the word, so add a ' ' (space) to var 't'
 					 if (j==0) {
-						 t += ' ';
+						 input = input.concat("\s");
 					 }
 				 } 
 			}			
-			System.out.println("Output (more than 1 word): \'" + t + "\'");
+			System.out.println("Output (more than 1 word): \'" + input + "\'");
 		}
 		
 		//If input string does not contain space, which means it is a single word
 		else {
 			
-			//Since 't' has the input word, we will use var 'p' to store the output
+			//Since 'input' has the input word, we will use var 'p' to store the output
 			p = "";
 			
 			//For the input word, navigate the characters of the string in reverse and keep adding it to var 'p'
-			for (int j = t.length()-1;j>=0;j--) {
-				p = p + t.charAt(j); 
+			for (int j = input.length()-1;j>=0;j--) {
+				p = p + input.charAt(j); 
 			} 
 			System.out.println("Output (Single word): \'" + p + "\'");
 		}
