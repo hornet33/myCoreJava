bb3ac308898169977ace3396d805a636e185164a
rahul_pradhan@outlook.com <rahul_pradhan@outlook.com>
2021-11-28 15:56:17 +0530

    Added BinarySearch

--- src/interviewPrep/searching/BinarySearch.java
+++ src/interviewPrep/searching/BinarySearch.java
@@ -1,4 +1,55 @@
 package interviewPrep.searching;
 
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] inputList = {12, 16, 24, 27, 28, 34, 59, 71}; //Pre-requisite to have the input search list to be sorted
+        int searchNum = 28;
+
+        System.out.print("Input Array: ");
+        printList(inputList);
+
+        int indexFound = doBinarySearch(inputList, 0, inputList.length, searchNum);
+        if (indexFound >= 0) {
+            System.out.println("Number " + searchNum + " found at index " + indexFound);
+        } else if (indexFound == -1) {
+            System.out.println("Number " + searchNum + " not found in the array");
+        }
+    }
+
+    public static int doBinarySearch(int[] inputArray, int lowerBound, int upperBound, int searchNum) {
+
+        //Check to ensure array limits are not exceeded for both lower and upper bounds
+        if (upperBound > lowerBound) {
+
+            //Calculate the middle index to search for this iteration
+            int mid = lowerBound + (upperBound - lowerBound) / 2;
+
+            System.out.println("lowerBound = " + lowerBound + " | upperBound = " + upperBound +
+                    " | mid = " + mid + " | input[mid] = " + inputArray[mid]);
+
+            //If searchNum < array[mid] then focus on left side of the array - lowerBound to mid-1
+            if (searchNum < inputArray[mid]) {
+                //Recursive call to method
+                return (doBinarySearch(inputArray, lowerBound, mid - 1, searchNum));
+            }
+            //If searchNum > array[mid] then focus on right side of the array - mid+1 to upperBound
+            else if (searchNum > inputArray[mid]) {
+                //Recursive call to method
+                return (doBinarySearch(inputArray, mid + 1, upperBound, searchNum));
+            }
+            //If searchNum == array[mid] return the index 'mid'
+            else if (inputArray[mid] == searchNum) {
+                return mid; //Element found at this position
+            }
+        }
+        return -1; //Element not found
+    }
+
+    //Static method to print the integer array
+    public static void printList(int[] input) {
+        for (int i : input) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 }
