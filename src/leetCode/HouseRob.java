34ad4712e46e1ee9fca5a701b32ccfc41f37bb35
arch_hornet <Acce$$Denied1>
2021-12-02 23:10:49 +0530

    Removed recursion logic

--- src/leetCode/HouseRob.java
+++ src/leetCode/HouseRob.java
@@ -3,38 +3,6 @@ package leetCode;
 import java.util.Arrays;
 
 public class HouseRob {
-    private int[] memo;
-
-    public int robRecursive(int[] nums) {
-
-        this.memo = new int[100];
-
-        // Fill with sentinel value representing not-calculated recursions.
-        Arrays.fill(this.memo, -1);
-
-        return this.robFrom(0, nums);
-    }
-
-    private int robFrom(int i, int[] nums) {
-
-        // No more houses left to examine.
-        if (i >= nums.length) {
-            return 0;
-        }
-
-        // Return cached value.
-        if (this.memo[i] > -1) {
-            return this.memo[i];
-        }
-
-        // Recursive relation evaluation to get the optimal answer.
-        int ans = Math.max(this.robFrom(i + 1, nums), this.robFrom(i + 2, nums) + nums[i]);
-
-        // Cache for future use.
-        this.memo[i] = ans;
-        return ans;
-    }
-
     public int myRobLogic(int[] nums) {
         int maxMoneyPath1 = nums[0];
         int maxMoneyPath2 = Math.max(maxMoneyPath1, nums[1]);
@@ -45,7 +13,6 @@ public class HouseRob {
             maxMoneyPath1 = maxMoneyPath2;
             maxMoneyPath2 = maxMoney;
         }
-
         return (maxMoney);
     }
 
