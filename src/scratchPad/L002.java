dc5a91fd27a406df9e95b2c48eda740d2435957c
arch_hornet <rahul_pradhan@outlook.com>
2021-12-11 11:23:34 +0530

    Adding scratchpad programs

---
+++ src/scratchPad/L002.java
@@ -0,0 +1,21 @@
+package scratchPad;
+
+import java.util.HashMap;
+
+public class L002 {
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> pantry = new HashMap<>();
+
+        pantry.put("Apples", 3);
+        pantry.put("Oranges", 2);
+
+        int currentApples = pantry.get("Apples");
+        pantry.put("Apples", currentApples + 4);
+
+        System.out.println(pantry.get("Apples"));
+
+        pantry.put("Apples", 10);
+        System.out.println(pantry.toString());
+    }
+}
