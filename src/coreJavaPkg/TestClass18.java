ee2699c6abb157561126163e186dde7899a861ab
rahul_pradhan@outlook.com <rahul_pradhan@outlook.com>
2022-02-17 11:20:27 +0530

    Program to find if two strings are anagrams or not

--- src/coreJavaPkg/TestClass18.java
+++ src/coreJavaPkg/TestClass18.java
@@ -5,19 +5,25 @@ package CoreJavaPkg;
 
 public class TestClass18 {
     public static Boolean isAnagram(String[] values) {
-        char[] v1 = values[0].toCharArray();
-        int l1 = values[0].length() - 1;
-        int l2 = values[1].length() - 1;
 
+        char[] v1 = values[0].toCharArray(); //Store the first string as a char[]
+        int l1 = values[0].length() - 1; //Get the length of the first string
+        int l2 = values[1].length() - 1; //Get the length of the second string
+
+        //Length of both strings have to be the same - if not, return false
         if (l1 != l2) {
             return false;
         }
 
+        //Loop through the length of the first string
         for (int i = 0; i <= l1; i++) {
+
+            //If the character in first string v1 is not found in second string (values[1]), return false
             if (values[1].indexOf(v1[i]) == -1) {
                 return false;
             }
         }
+        //Return true since lengths are the same and all characters in both strings, return true
         return true;
     }
 
