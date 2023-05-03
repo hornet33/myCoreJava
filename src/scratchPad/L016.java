9067d74e24226b126de6d51653e0cc77843b10e6
rahulpradhan <rahul_pradhan@outlook.com>
2023-05-03 19:02:11 +0530

    May2023 Commit

---
+++ src/scratchPad/L016.java
@@ -0,0 +1,15 @@
+//Java program to find the Euclidean distance between a set of (x,y) coordinates
+
+package scratchPad;
+
+public class L016 {
+    public static void main(String[] args) {
+        int[][] coordinates = new int[][]{{1, 2}, {3, 4}};
+        System.out.println(coordinates);
+        for (int[] i : coordinates) {
+            for (int j : i) {
+                System.out.println(j);
+            }
+        }
+    }
+}
