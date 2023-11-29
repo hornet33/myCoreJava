03cd91f137171f33f18d91726f69f46a6b51a661
arch_hornet <rahul_pradhan@outlook.com>
2023-11-29 08:35:08 +0530

    Print a list of ALL non-repeated chars

--- src/coreJavaPkg/TestClass07.java
+++ src/coreJavaPkg/TestClass07.java
@@ -11,17 +11,14 @@ import java.util.List;
 public class TestClass07 {
 
 	public static void main(String[] args) {
-		String input = "application programming";
+		String input = "application programming using Java17";
 		
-		//For case insensitive comparison, converting the input string to all lower case
+		//For case-insensitive comparison, converting the input string to all lower case
 		String inputAllLower = input.toLowerCase();
 
 		//Create a list to add all the non-repeated characters in the string
 		List<Character> nonRepChars = new ArrayList<>();
 
-		//Set a flag to check if first non-repeated character is found
-		boolean firstNonRepChar = true;
-		
 		//Loop through the input String characters
 		for(int i=0;i<input.length();i++) {
 
@@ -32,18 +29,11 @@ public class TestClass07 {
 			
 			//Look for a character which has first and last index equal which means 1 occurrence (non-repeated)
 			if(inputAllLower.indexOf(inputAllLower.charAt(i)) == inputAllLower.lastIndexOf(inputAllLower.charAt(i))) {
-
-				if(firstNonRepChar){
-					//Print the output
-					System.out.println("First non-repeated character in the string '" + input + "' is '"+ input.charAt(i) + "'.");
-					//Set the flag to false since first non-repeated character is found
-					firstNonRepChar = false;
-				}
-
 				//Add the non-repeated character to the nonRepChar list
 				nonRepChars.add(input.charAt(i));
 			}
 		}
-		System.out.println("List of all non-repeated characters in the string:" + nonRepChars);
+		System.out.println("First non-repeated character in the string '" + input + "' is '" + nonRepChars.get(0) + "'");
+		System.out.println("List of all non-repeated characters in the string: " + nonRepChars);
 	}
 }
