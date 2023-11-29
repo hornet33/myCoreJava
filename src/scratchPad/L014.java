065eee7ac64abbe68ac48ba217f64346a58d6da2
arch_hornet <rahul_pradhan@outlook.com>
2023-11-29 07:25:26 +0530

    Changed input string

--- src/scratchPad/L014.java
+++ src/scratchPad/L014.java
@@ -4,14 +4,14 @@ import java.util.*;
 
 public class L014 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter words separated by commas: ");
-        String[] inputSentence = sc.next().split(",");
+
+        String input = "Quick Brown Fox Field Brown Black Blue Fox Test Brown Blue Sky";
+        String[] inputSentence = input.split(" ");
 
         System.out.println("Input: " + Arrays.toString(inputSentence));
 
         //To count the number of times a word comes in a sentence
-        Map<String, Integer> wordCountMap = new HashMap<>();
+        Map<String, Integer> wordCountMap = new LinkedHashMap<>();
         for(String s: inputSentence) {
             if(wordCountMap.containsKey(s)){
                 wordCountMap.put(s, wordCountMap.get(s) + 1);
@@ -23,7 +23,7 @@ public class L014 {
         System.out.println("Word Count: " + wordCountMap);
 
         //To remove redundant words from the input
-        Set<String> uniqueWords = new HashSet<>();
+        Set<String> uniqueWords = new LinkedHashSet<>();
         Collections.addAll(uniqueWords, inputSentence);
         System.out.println("Unique Words: " + uniqueWords);
     }
