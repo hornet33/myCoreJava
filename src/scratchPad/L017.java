d269d4d9e3848cb2724e0d0f6f668e423a449bf7
rahulpradhan <rahul_pradhan@outlook.com>
2023-12-04 18:05:57 +0530

    Dec2023 Commit

--- src/scratchPad/L017.java
+++ src/scratchPad/L017.java
@@ -2,10 +2,24 @@
 
 package scratchPad;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class L017 {
     public static void main(String[] args) {
         String str = "rahul pradhan";
         String reverse = new StringBuilder(str).reverse().toString();
-        System.out.printf("Actual Word: '%s' | Word after reversing: '%s'", str, reverse);
+        System.out.printf("Actual Word: '%s' | Word after reversing: '%s' \n", str, reverse);
+
+        System.out.println();
+
+        //Get the first characters from the reversed words
+        String[] sArr = reverse.split(" ");
+        System.out.println(Arrays.toString(sArr));
+        StringBuilder firstCharStr = new StringBuilder();
+        for(String s: sArr){
+            firstCharStr.append(s.charAt(0));
+        }
+        System.out.println(firstCharStr);
     }
 }
